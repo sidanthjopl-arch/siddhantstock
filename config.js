5c895e459cb34cd02a26934fa6b17878629f63c4
sidanthjopl-arch <sidanthjopl@gmail.com>
2026-06-17 13:14:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
   // Paste the Web App URL you get after deploying the Apps Script
   // (see README.md). It will look like:
   // https://script.google.com/macros/s/XXXXXXXX/exec
-  APPS_SCRIPT_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz9mVWKldfPN2hF_cSts3hX3cwRKdMJGR2dnUCcH4oY6i3O8MCHBjkRD6NmxI3tQJL8hA/exec",
 
   // Published CSV link for "Sheet1" (column A = SKU, column B = image link)
   SKU_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMYiDx_noTYEQffm5508rXIcFyuHSecEKRlpVXGYIhmYWhWZgSRXsHLoOQTeuy7PG8D-7RF8Op-0Uq/pub?output=csv"
